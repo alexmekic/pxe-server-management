82be6fff4fc425745226afcf33cca94a166d4cf1
Alex Mekic <59836295+kuroyoshi10@users.noreply.github.com>
2021-01-18 09:58:29 -0500

    Merged clonezilla_updater and freebsd_updater

--- adv_and_update_menu.py
+++ adv_and_update_menu.py
@@ -1,23 +1,30 @@
-import clonezilla_updater, freebsd_updater, change_server_config
+import pxe_updater, change_server_config
 import subprocess
+from zfs_pool import PXE
 
-def update_menu(clonezilla_ver, freebsd_version):
+def update_menu(management_ver, clonezilla_ver, freebsd_version):
     while True:
         print("\nPXE Server Update Menu\n")
-        print("1: Check updates for Clonezilla")
-        print("2: Revert previous Clonezilla version")
-        print("3: Check patch and package updates for FreeBSD (Reboot may be required)")
-        print("4: Check minor or major updates for FreeBSD (Reboot will be required)")
+        print("1: Check updates for PXE Management Application")
+        print("2: Check updates for Clonezilla")
+        # print("3: Revert to previous version of PXE Management Application")
+        print("3: Revert to previous version of Clonezilla")
+        print("4: Check patch and package updates (Reboot recommended)")
+        print("5: Check for OS upgrades (Reboot required)")
         print("0: Exit back to Main Menu\n")
-        update_prompt = input("LOAD: ")
+        update_prompt = input("> ")
         if update_prompt == "1":
-            clonezilla_updater.check_clonezilla_update(clonezilla_ver)
+            pxe_updater.check_management_update(management_ver)
         elif update_prompt == "2":
-            clonezilla_updater.revert_clonezilla()
+            pxe_updater.check_clonezilla_update(clonezilla_ver)
+        # elif update_prompt == "3":
+        #     pxe_updater.revert_pxe_management()
         elif update_prompt == "3":
-            freebsd_updater.check_package_patch_updates()
+            pxe_updater.revert_clonezilla()
         elif update_prompt == "4":
-            freebsd_updater.check_freebsd_updates(freebsd_version.split('-')[0])
+            pxe_updater.check_package_patch_updates()
+        elif update_prompt == "5":
+            pxe_updater.check_freebsd_updates(freebsd_version.split('-')[0])
         elif update_prompt == "0":
             break
         else:
@@ -28,12 +35,13 @@ def adv_menu(ip_addr, netmask_addr):
         print("\nPXE Advanced Menu\n")
         print("1: Change static IP address")
         print("2: Change DHCP IP address range")
+        #print("3: Flush DHCP Cache")
         print("3: Change admin account password")
-        print("4: Reset permissions on /pxe/images")
+        print("4: Reset permissions")
         print("5: Reboot Server")
         print("6: Shutdown Server")
         print("0: Exit back to Main Menu\n")
-        adv_prompt = input("LOAD: ")
+        adv_prompt = input("> ")
         if adv_prompt == "1":
             change_server_config.change_ip_address(ip_addr, netmask_addr)
         elif adv_prompt == "2":
@@ -44,11 +52,8 @@ def adv_menu(ip_addr, netmask_addr):
             print("Enter new admin Samba password for access of files on server from Windows.")
             subprocess.call('smbpasswd', shell=True)
         elif adv_prompt == "4":
-            subprocess.call('sudo chown -R admin:admin /pxe/images', shell=True)
-            subprocess.call('find /pxe/images -type d -print0 | sudo xargs -0 chmod 0755', shell=True)
-            subprocess.call('find /pxe/images -type f -print0 | sudo xargs -0 chmod 0644', shell=True)
-            subprocess.call('sudo chmod 753 /pxe/images', shell=True)
-            print("Permissions on /pxe/images reset successfully")
+            subprocess.call('sudo chmod 770 /' + PXE.zfs_pool + '/images', shell=True)
+            print("Permissions reset successfully")
         elif adv_prompt == "5":
             print("Rebooting server...")
             subprocess.call('sudo reboot', shell=True)
