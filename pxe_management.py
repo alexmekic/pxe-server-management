4c2a8f6ad8a291a47a0a7a9c9914e6128a05b456
Alex Mekic <59836295+kuroyoshi10@users.noreply.github.com>
2021-01-18 10:00:34 -0500

    Reworded menu and removed /pxe with user defined name of pxe directory

--- pxe_management.py
+++ pxe_management.py
@@ -1,9 +1,12 @@
 import retrieve_info, adv_and_update_menu, sync_images_boot_menu_files, list_images_and_entries
 import add_remove_edit_boot_entries, rename_delete_image
 import os, re, glob, sys, platform
+from zfs_pool import PXE
 
 def main():
+    management_ver = "1.9"
     if not retrieve_info.zfs_health():
+        print("Application terminated due to disk error detected in storage pool")
         sys.exit()
     while True:
         if platform.system() == "FreeBSD":
@@ -11,8 +14,8 @@ def main():
         #elif platform.system() == "Linux":
         #    linux_os = distro.linux_distribution()
         #    print("\nLinux version: " + linux_os[0] + ' ' + linux_os[1] + ' ' + linux_os[2], end='', flush=True)
-        if glob.glob('/pxe/tftp/clonezilla/Clonezilla-Live-Version'):
-            with open('/pxe/tftp/clonezilla/Clonezilla-Live-Version') as f:
+        if glob.glob(PXE.zfs_pool + '/tftp/clonezilla/Clonezilla-Live-Version'):
+            with open(PXE.zfs_pool + '/tftp/clonezilla/Clonezilla-Live-Version') as f:
                 clonezilla_ver = f.readline()
             clonezilla_ver = re.search('clonezilla-live-(.*)-amd64', clonezilla_ver).group(1)
             print("Clonezilla version: " + str(clonezilla_ver), end='', flush=True)
@@ -20,8 +23,8 @@ def main():
             print('Clonezilla not installed!')
         ip_addr, netmask_addr = retrieve_info.active_ip()
         print("\nCurrent static IP address: " + ip_addr + " netmask: " + netmask_addr)
-        free_space = os.popen("df -lh | grep /pxe | awk '{print $4}'").read()
-        print("Free space available in /pxe: " + free_space)
+        free_space = os.popen("df -lh | grep " + PXE.zfs_pool + " | awk '{print $4}'").read()
+        print("Free space available in " + PXE.zfs_pool + ": " + free_space)
         prompt = user_input()
         if prompt == 1:
             add_remove_edit_boot_entries.add_restore_entry()
@@ -44,7 +47,7 @@ def main():
         elif prompt == 10:
             sync_images_boot_menu_files.sync_server(ip_addr)
         elif prompt == 11:
-            adv_and_update_menu.update_menu(clonezilla_ver, platform.release())
+            adv_and_update_menu.update_menu(management_ver, clonezilla_ver, platform.release())
         elif prompt == 12:
             adv_and_update_menu.adv_menu(ip_addr, netmask_addr)
         elif prompt == 0:
@@ -56,21 +59,21 @@ def main():
 def user_input():
     while True:
         options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '0']
-        print("PXE Server Management v1.3\n")
-        print("1: Add New restore entry")
-        print("2: List current entries and restore images attached")
-        print("3: List restore images in server\n")
-        print("4: Change default entry")
-        print("5: Change attached restore image in entry")
-        print("6: Rename restore image in server\n")
-        print("7: Revert change in menu file to previous state")
-        print("8: Delete entry")
-        print("9: Delete image\n")
-        print("10: Sync restore images and menu file to another server\n")
-        print("11: Check Clonezilla and FreeBSD updates")
-        print("12: Enter Advanced Menu\n")
+        print("PXE Server Management Application v2.0 Beta 1\n")
+        print("1: Create new boot entry")
+        print("2: List boot entries")
+        print("3: List restore images\n")
+        print("4: Change default boot entry")
+        print("5: Change restore image from boot entry")
+        print("6: Rename restore image\n")
+        print("7: Revert to previous state of boot entry file")
+        print("8: Delete boot entry")
+        print("9: Delete restore image\n")
+        print("10: Sync images and boot entry to another server\n")
+        print("11: Check for updates")
+        print("12: Advanced Options\n")
         print("0: Exit to shell\n")
-        prompt = (input("LOAD: "))
+        prompt = (input("> "))
         if prompt == "0":
             sys.exit()
         elif prompt in options:
@@ -78,5 +81,4 @@ def user_input():
         else:
             print("Invalid Selection\n")
 
-if __name__ == "__main__":
-    main()
+main()
