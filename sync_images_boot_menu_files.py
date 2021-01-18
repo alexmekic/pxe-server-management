24abb05db0b212637a2907fdcc01a522e7138693
Alex Mekic <59836295+kuroyoshi10@users.noreply.github.com>
2021-01-18 10:04:19 -0500

    (Currently beta)

--- sync_images_boot_menu_files.py
+++ sync_images_boot_menu_files.py
@@ -1,7 +1,64 @@
 import change_server_config
 import subprocess, os, shutil, dirsync, fileinput
+from zfs_pool import PXE
+from pathlib import Path
 
-def sync_server(main_server_ip):
+def mount_network_dir(backup_storage_pool, subdirectory, second_server_ip):
+    print("Mounting " + backup_storage_pool + " from " + second_server_ip + "...", end='', flush=True)
+    os.mkdir(PXE.zfs_pool + '/nfsmount')
+    exit_code = subprocess.call('sudo mount_nfs -o retrycnt=1 ' + second_server_ip + ":" + backup_storage_pool + "/" + subdirectory + " " + PXE.zfs_pool + "/nfsmount", shell=True)
+    if exit_code != 0:
+        print("failed")
+        print("Unable to mount network directory from " + second_server_ip)
+        os.rmdir(PXE.zfs_pool + '/nfsmount')
+        return False
+    else:
+        print("done")
+        return True
+
+#fix this
+# def copy_entry_file(main_server_ip, second_server_ip, direction):
+#     try:
+#         if direction == 'upload':
+#             print("Uploading boot menu file to " + second_server_ip + "...", end='', flush=True)
+#             filepath = Path(PXE.zfs_pool + '/nfsmount/boot.ipxe')
+#             filepath.rename(filepath.with_suffix('.bak'))
+#             subprocess.call('rsync ')
+#             with fileinput.FileInput(PXE.zfs_pool + '/nfsmount/boot.ipxe', inplace=True) as file:
+#                 for line in file:
+#                     print(line.replace(main_server_ip, second_server_ip), end='')
+#             subprocess.call("sudo umount " + PXE.zfs_pool + "/nfsmount", shell=True)
+#             os.rmdir(PXE.zfs_pool + '/nfsmount')
+#             print("done")
+#         elif direction == "download":
+#             print("Downloading boot menu file from " + second_server_ip + "...", end='', flush=True)
+            
+#     except:
+#         print("failed")
+#
+
+def sync_images():
+    try:
+        subprocess.call('find /' + PXE.zfs_pool + '/images -type d -print0 | sudo xargs -0 chmod 0770', shell=True)
+        subprocess.call('find /' + PXE.zfs_pool + '/images -type f -print0 | sudo xargs -0 chmod 0660', shell=True)
+        subprocess.call('find /' + PXE.zfs_pool + '/nfsmount -type d -print0 | sudo xargs -0 chmod 0770', shell=True)
+        subprocess.call('find /' + PXE.zfs_pool + '/nfsmount -type f -print0 | sudo xargs -0 chmod 0660', shell=True)
+        print("Syncing images from main to backup...", end='', flush=True)
+        dirsync.sync(PXE.zfs_pool + '/images', PXE.zfs_pool + '/nfsmount', 'sync')
+        print("done")
+        print("Syncing images from backup to main...", end='', flush=True)
+        dirsync.sync(PXE.zfs_pool + '/nfsmount', PXE.zfs_pool + '/images', 'sync')
+        print("done")
+        print("Unmounting...", end='', flush=True)
+        subprocess.call('sudo umount ' + PXE.zfs_pool + '/nfsmount', shell=True)
+        os.rmdir(PXE.zfs_pool + '/nfsmount')
+        print("done")
+    except:
+        subprocess.call("sudo umount " + PXE.zfs_pool + "/nfsmount", shell=True)
+        os.rmdir(PXE.zfs_pool + '/nfsmount')
+        print("failed")
+
+def sync_server(main_server_ip, sync_type):
     while True:
         second_server_ip = input("Enter IP address of secondary/backup server (x to exit): ")
         if second_server_ip == "x":
@@ -11,48 +68,41 @@ def sync_server(main_server_ip):
             print("Checking if " + second_server_ip + " is reachable...")
             ping_response = os.system("ping -c 5 " + second_server_ip)
             if ping_response == 0:
-                print("Server is reachable. Syncing images and boot menu file...")
-                subprocess.call('sudo mkdir /pxe/nfsmount', shell=True)
-                print("Attempting to mount boot menu directory from " + second_server_ip + "...", end='', flush=True)
-                exit_code = subprocess.call('sudo mount_nfs -o retrycnt=1 ' + second_server_ip + ":/pxe/tftp /pxe/nfsmount", shell=True)
-                if exit_code != 0:
-                    print("failed")
-                    print("Mounting from " + second_server_ip + " for updating boot menu file failed.")
-                else:
-                    print("done")
-                    print("Updating boot menu file...", end='', flush=True)
-                    shutil.copy2("/pxe/nfsmount/boot.ipxe", "/pxe/nfsmount/boot.ipxe.bak")
-                    os.remove("/pxe/nfsmount/boot.ipxe")
-                    shutil.copy2("/pxe/tftp/boot.ipxe", "/pxe/nfsmount/boot.ipxe")
-                    print("done")
-                with fileinput.FileInput('/pxe/nfsmount/boot.ipxe', inplace=True) as file:
-                    for line in file:
-                        print(line.replace(main_server_ip, second_server_ip), end='')
-                subprocess.call("sudo umount /pxe/nfsmount", shell=True)
-                print("Attempting to mount images directory from " + second_server_ip + "...", end='', flush=True)
-                exit_code = subprocess.call('sudo mount_nfs -o retrycnt=1 ' + second_server_ip + ":/pxe/images /pxe/nfsmount", shell=True)
-                if exit_code != 0:
-                    print("failed")
-                    print("Mounting from " + second_server_ip + " for updating images failed.")
-                else:
-                    print("done")
-                    print("Syncing images from main to backup...", end='', flush=True)
-                    subprocess.call('sudo chown -R admin:admin /pxe/images', shell=True)
-                    subprocess.call('find /pxe/images -type d -print0 | sudo xargs -0 chmod 0755', shell=True)
-                    subprocess.call('find /pxe/images -type f -print0 | sudo xargs -0 chmod 0644', shell=True)
-                    dirsync.sync('/pxe/images', '/pxe/nfsmount', 'sync')
-                    print("done")
-                    print("Syncing images from backup to main...", end='', flush=True)
-                    dirsync.sync('/pxe/nfsmount', '/pxe/images', 'sync')
-                    subprocess.call('sudo chown -R admin:admin /pxe/images', shell=True)
-                    subprocess.call('find /pxe/images -type d -print0 | sudo xargs -0 chmod 0755', shell=True)
-                    subprocess.call('find /pxe/images -type f -print0 | sudo xargs -0 chmod 0644', shell=True)
-                    subprocess.call('sudo chmod 753 /pxe/images', shell=True)
-                    subprocess.call('sudo umount /pxe/nfsmount', shell=True)
-                    subprocess.call('sudo rm -r /pxe/nfsmount', shell=True)
-                    print("done")
+                print("Success")
+                while True:
+                    backup_storage_pool = input("Enter full path of storage pool from backup server (0 to exit): ")
+                    if backup_storage_pool == "0":
+                        print("Operation aborted")
+                        break
+                    else:
+                        if sync_type == "1":
+                            mount_network_dir(backup_storage_pool, "images", second_server_ip)
+                            sync_images()
+                        # elif sync_type == "2":
+                        #     mount_network_dir(backup_storage_pool, "tftp", second_server_ip)
+                        #     copy_entry_file(main_server_ip, second_server_ip, "upload")
+                        # elif sync_type == "3":
+                        #     mount_network_dir(backup_storage_pool, "tftp", second_server_ip)
+                        #     copy_entry_file(main_server_ip, second_server_ip, "download")
+                        break
                 break
             else:
                 print("Server is not reachable. Check connection and network settings")
         else:
             print("Invalid IP address")
+
+def sync_menu(main_server_ip):
+    while True:
+        options = ['1', '2', '3', '0']
+        print("PXE Server Syncing Menu\n")
+        print("1: Sync images with another server")
+        # print("2: Upload boot menu file to another server")
+        # print("3: Download boot menu file from another server")
+        print()
+        sync_prompt = input("> ")
+        if sync_prompt == '0':
+            break
+        elif sync_prompt in options:
+            sync_server(main_server_ip, sync_prompt)
+        else:
+            print("Invalid Option") 
